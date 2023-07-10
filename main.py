3bb7983a758837328ab5a3ff5d7cf32d117a1ac4
Antglo <100139359+Antglo@users.noreply.github.com>
2023-07-10 21:20:37 +0000

    Added more conversions.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #antglo 7/10/2023
 #Still developing
-#I intend for this program to contain a bunch of different conversion rates, I am using this for practice as I work towards my PCEP
+#I intend for this program to contain a bunch of different conversion formulas/rates, I am using this for practice as I work towards my PCEP
 
 #conversion for miles to kilometers
 def miles_to_kilometers(miles):
@@ -13,8 +13,22 @@ def kilometeters_to_miles(km):
     km /= 1.609
     km_converted = km
     print(round(init_km, 2), "kilometers is equal to:", round(km_converted, 2), "miles.")
+    
+#conversion for feet to miles
+def feet_to_miles(ft):
+    ft /= 5280
+    ft_converted = ft
+    print(round(init_ft, 3), "feet is equal to:", round(ft_converted, 3), "miles")
+    
+#conversion for miles to feet
+def miles_to_feet(miles_2_ft):
+    miles_2_ft *= 5280
+    m_2_ft_converted = miles_2_ft
+    print(round(init_miles_2_ft, 2), "miles is equal to:", round(m_2_ft_converted, 2), "feet")
+    
 
 if __name__ == "__main__":
-    init_km = float(input("Enter the amount of km you would like to convert: "))
+    #init_km = float(input("Enter the amount of km you would like to convert: "))
     # miles_to_kilometers(init_miles)
-    kilometeters_to_miles(init_km)
+    init_miles_2_ft = float(input("Enter the amount of miles: "))
+    miles_to_feet(init_miles_2_ft)
